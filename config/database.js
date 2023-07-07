c162a04a242394834dba858ab76ae17165cd30b8
Rodrigo Zandonadi dbsOOp <rodrigo.zandonadi@dbsnoop.com>
2023-07-07 17:41:15 -0300

    New test_user script in package.json

--- config/database.js
+++ config/database.js
@@ -19,7 +19,7 @@ if (process.env.NODE_ENV === 'production') {
   connectDatabase = async () => { 
     try {
       const MONGODB_URI = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@cluster0.ttlubgg.mongodb.net/?retryWrites=true&w=majority`;
-      await mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+      await mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
       console.log('Database connected! By the Power of Grayskull!!!I have the power!');
     } catch (error) {
       console.error('Oh No, Zordon! Error in Morphin Time:', error);
---
+++ scripts/test_user.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+API_URL="http://localhost:4000" 
+USERNAME="teste_user"
+PASSWORD="senha123"
+EMAIL="exemplo@example.com"
+
+curl -X POST -H "Content-Type: application/json" -d '{
+  "username": "'"$USERNAME"'",
+  "password": "'"$PASSWORD"'",
+  "email": "'"$EMAIL"'"
+}' $API_URL/users
