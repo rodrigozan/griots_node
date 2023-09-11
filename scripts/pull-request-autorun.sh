f6063cb314372ebe9efbf7bb7fcf82b2d774937b
rodrigozan <rodzandonadi@gmail.com>
2023-09-11 14:08:45 +0000

    Add errors outputs in pull-request-autorun.sh

--- scripts/pull-request-autorun.sh
+++ scripts/pull-request-autorun.sh
@@ -5,10 +5,12 @@ REPO_DIR="/app/www/"
 
 # Função para executar um pull request
 execute_pull_request() {
+  echo "Entrou na função"
   cd $REPO_DIR
 
   # Tenta realizar o pull
   git pull origin master
+  echo "Git pull realizado"
 
   # Verifica se houve conflitos
   if [ $? -ne 0 ]; then
@@ -23,13 +25,16 @@ execute_pull_request() {
     # Tente novamente o pull
     git pull origin master
   fi
+  echo "Script executed successfully"
 }
 
 # Monitora o diretório em busca de mudanças
 inotifywait -m -r -e create,modify,delete $REPO_DIR | while read path action file; do
   # Verifica se a ação é um arquivo de pull request pendente
+  echo "Verificando se a ação é um arquivo de pull request pendente"
   if [[ $file == "PULL_REQUEST_PENDING" ]]; then
     # Executa o pull request
     execute_pull_request
   fi
+  echo "Script executed successfully"
 done
